6f37bd36f6bbc76c91356cfbdbe62e8170078821
Niall Lyons <niall.lyons4@mail.dcu.ie>
2016-12-07 00:42:50 +0000

    Create ID4.py

---
+++ ID4.py
@@ -0,0 +1,9 @@
+def largePalindrome():
+  container = []
+  for i in range(100, 999):
+    for j in range(100, 999):
+      num = i * j
+      if str(num) == str(num)[::-1]:
+        container.append(num)
+
+	print(max(container))
